a4bbd00f818e956c91fa606b781891443a5293bc
Listandsell2021 <84175604+Listandsell2021@users.noreply.github.com>
2024-12-18 15:10:46 +0100

    view_change

--- scrap/urls.py
+++ scrap/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from scrap import views
 
 urlpatterns = [
-    path('', views.form_request,name='form_request'),   
-  
-    
-    
+    # path('', views.form_request,name='form_request'),   
+     path('', views.hello_view, name='hello'),
 ]
--- scrap/views.py
+++ scrap/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # from scrap.models import Websites_Model
 
-def form_request(request):
-    # website_name = Websites_Model.objects.all()
-    return render(request, 'form.html')
+# def form_request(request):
+#     # website_name = Websites_Model.objects.all()
+#     return render(request, 'form.html')
+
+def hello_view(request):
+    return HttpResponse("Hello")
